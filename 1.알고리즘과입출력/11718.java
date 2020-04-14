26547f426c8ee562c93253a5a762600f1fd8d4f6
shinjihye <wlgp2500@naver.com>
2020-04-15 01:12:08 +0900

    그대로 출력하기

---
+++ 1.알고리즘과입출력/11718.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import javax.swing.plaf.synth.SynthSpinnerUI;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str="";
+		Scanner scan=new Scanner(System.in);
+		while(scan.hasNextLine()) {
+			str=scan.nextLine();
+			System.out.println(str);
+		}
+
+		
+	
+	}
+
+}

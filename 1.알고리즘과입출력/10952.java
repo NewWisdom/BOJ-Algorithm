cb592cdb9a5ab617b89f38f8dd7e323bac4707dc
shinjihye <wlgp2500@naver.com>
2020-04-14 23:54:40 +0900

    Create 10952.java

---
+++ 1.알고리즘과입출력/10952.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int A=0,B=0;
+		int arr[];
+		Scanner scan =new Scanner(System.in);
+		while ((A==0)&&(B==0)) {
+			A = scan.nextInt();
+			B = scan.nextInt();
+			System.out.println(A+B);
+		}
+	
+	}
+
+}

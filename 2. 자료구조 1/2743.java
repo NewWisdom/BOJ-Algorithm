423a9af6fc219ab84b7223e4b9cca5cee3d7ca18
wlgp2500 <43840561+wlgp2500@users.noreply.github.com>
2020-05-02 19:06:44 +0900

    단어 길이 재기

---
+++ 2. 자료구조 1/2743.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+import javax.swing.plaf.synth.SynthSeparatorUI;
+import javax.swing.plaf.synth.SynthSpinnerUI;
+import javax.swing.text.html.HTMLDocument.Iterator;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception{
+		// TODO Auto-generated method stub
+
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		String str=br.readLine();
+		System.out.println(str.length());
+
+
+		
+	}
+
+}
